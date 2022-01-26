cf503179a0f542f1be4523c822b3a421c12ba7b9
Jason Biegel <me@jasonbiegel.com>
2022-01-25 23:08:45 -0500

    get inches

--- shouldwater.go
+++ shouldwater.go
@@ -2,10 +2,36 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net/http"
 	"os"
+	"encoding/json"
+	"math"
 )
+
+
+type NcdcResponse struct {
+	Metadata struct {
+		Resultset struct {
+			Offset int `json:"offset"`
+			Count  int `json:"count"`
+			Limit  int `json:"limit"`
+		} `json:"resultset"`
+	} `json:"metadata"`
+	Results []struct {
+		Date       string `json:"date"`
+		Datatype   string `json:"datatype"`
+		Station    string `json:"station"`
+		Attributes string `json:"attributes"`
+		Value      int    `json:"value"`
+	} `json:"results"`
+}
+
+const PrecipitationDataType = "PRCP"
+
+// convertToInch will translate the NCDC value for precipitation, which is tenths of mm, to inches
+func convertToInch(value int) float64 {
+	return math.Round(float64(value) / 10 / 25.4 * 100) / 100
+}
 
 func main() {
 	token := os.Getenv("TOKEN")
@@ -15,9 +41,23 @@ func main() {
 	req, _ := http.NewRequest("GET", "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=GHCND&stationid=GHCND:USW00094728&startdate=2022-01-16&enddate=2022-01-22", nil)
 	req.Header.Add("token", token)
 
-	resp, _ := client.Do(req)
-	defer resp.Body.Close()
-	body, _ := io.ReadAll(resp.Body)
+	rawResp, _ := client.Do(req)
+	defer rawResp.Body.Close()
 
-	fmt.Println(string(body))
+	var resp NcdcResponse
+	err := json.NewDecoder(rawResp.Body).Decode(&resp)
+	if err != nil {
+        fmt.Println(err.Error(), http.StatusBadRequest)
+        return
+    }
+
+	totalPrecip := 0
+
+	for _, result := range resp.Results {
+		if result.Datatype == PrecipitationDataType {
+			totalPrecip += result.Value
+		}
+	}
+
+	fmt.Println(convertToInch(totalPrecip))
 }

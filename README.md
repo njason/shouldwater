fde9c184f39226096b47f36535d6f2c132ab89a0
Jason Biegel <me@jasonbiegel.com>
2022-02-05 11:34:34 -0500

    updates

--- README.md
+++ README.md
@@ -2,13 +2,29 @@
 
 Checks recent precipiation data in your area and decides whether it's time to water unestablished trees which are less than two years old.
 
+[Guide for watering trees](https://arbordayblog.org/treecare/how-to-properly-water-your-trees/)
+
+## Setup
+
+First, [obtain a token](https://www.ncdc.noaa.gov/cdo-web/token)
+
+Next, copy the `config-template.yaml` into a new file `config.yaml` and update the file to contain your token.
+
+## Running
+
+You need to pass the station ID as an argument into the program. [Find your station ID here](https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt)
+
+For example, the station ID for Central Park is `USW00094728`
+
 Example usage:
 ```
-TOKEN=<web token> STATION=USW00094728 ./shouldwater
+go run shouldwater.go USW00094728
 ```
 
-[Get Web Token](https://www.ncdc.noaa.gov/cdo-web/token)
+## Known Issues
 
-[Find your station ID](https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt)
+The NCDC data is not always up to date and sometimes days are missing. However, when you retrieve data through another way it can contain data that NCDC doesn't have. Example request: 
 
-[Guide for watering trees](https://arbordayblog.org/treecare/how-to-properly-water-your-trees/)
+```
+https://www.ncei.noaa.gov/access/services/data/v1?dataset=daily-summaries&startDate=2022-01-29&endDate=2022-02-04&stations=USW00094728&format=json
+```
--- shouldwater.go
+++ shouldwater.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"math"
 	"net/http"
 	"os"
 	"time"
+
+	"gopkg.in/yaml.v3"
 )
+
+type Config struct {
+	Token string `yaml:"token"`
+}
 
 type NcdcResponse struct {
 	Metadata struct {
@@ -27,7 +34,7 @@ type NcdcResponse struct {
 }
 
 const PrecipitationDataType = "PRCP"
-const WateringThreshold = 1 // in inches
+const WateringThreshold = 1  // in inches
 
 // convertToInch will translate the NCDC value for precipitation, which is tenths of mm, to inches
 func convertToInch(value int) float64 {
@@ -38,22 +45,47 @@ func getQueryFormat(time time.Time) string {
 	return fmt.Sprintf("%d-%02d-%02d", time.Year(), time.Month(), time.Day())
 }
 
+func loadConfig() Config {
+	f, err := os.Open("config.yaml")
+	if err != nil {
+		fmt.Println(err.Error(), http.StatusBadRequest)
+	}
+	defer f.Close()
+
+	var config Config
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(&config)
+	if err != nil {
+		fmt.Println(err.Error(), http.StatusBadRequest)
+	}
+
+	return config
+}
+
 func main() {
-	token := os.Getenv("TOKEN")
-	stationId := os.Getenv("STATION")
+	flag.Parse()
+	if flag.NArg() == 0 {
+		fmt.Println("Please provide station ID.")
+		os.Exit(1)
+	}
+
+	config := loadConfig()
+	stationId := flag.Arg(0)
 
 	client := http.DefaultClient
 
-	end := time.Now()
-	start := end.AddDate(0, 0, -7)
+	today := time.Now()
+	end := today.AddDate(0, 0, -1)
+	start := end.AddDate(0, 0, -6)
 
 	req, _ := http.NewRequest("GET", "https://www.ncdc.noaa.gov/cdo-web/api/v2/data", nil)
-	req.Header.Add("token", token)
+	req.Header.Add("token", config.Token)
 	query := req.URL.Query()
 	query.Add("datasetid", "GHCND")
 	query.Add("stationid", fmt.Sprintf("GHCND:%s", stationId))
 	query.Add("startdate", getQueryFormat(start))
 	query.Add("enddate", getQueryFormat(end))
+	query.Add("limit", "1000")
 	req.URL.RawQuery = query.Encode()
 
 	rawResp, _ := client.Do(req)
@@ -67,19 +99,25 @@ func main() {
 	}
 
 	totalPrecipRaw := 0
+	dayCounter := 0
 
 	for _, result := range resp.Results {
 		if result.Datatype == PrecipitationDataType {
 			totalPrecipRaw += result.Value
+			dayCounter++
 		}
 	}
 
-	totalPrecip := convertToInch(totalPrecipRaw)
-
-	fmt.Printf("It's rained %.2f inches in the last week\n", totalPrecip)
-	if totalPrecip < WateringThreshold {
-		fmt.Println("You should water the trees.")
+	if dayCounter < 7 {
+		fmt.Println("Insufficient data to total rain")
 	} else {
-		fmt.Println("No need to water the trees, it's rained enough.")
+		totalPrecip := convertToInch(totalPrecipRaw)
+		
+		fmt.Printf("It's rained %.2f inches in the last week\n", totalPrecip)
+		if totalPrecip < WateringThreshold {
+			fmt.Println("You should water the trees.")
+		} else {
+			fmt.Println("No need to water the trees, it's rained enough.")
+		}
 	}
 }

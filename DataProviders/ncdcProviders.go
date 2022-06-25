69bad2ae7228d99fd11a22265b3260efa41b0c68
alexgoldschmidt <alexgoldschmidt1@gmail.com>
2022-06-25 00:41:36 -0400

    Broke out packages for Models and DataProvider.

---
+++ DataProviders/ncdcProviders.go
@@ -0,0 +1,132 @@
+package dataproviders
+
+import (
+	"encoding/json"
+	"net/http"
+	"fmt"
+	"math"
+	"os"
+	"time"
+	"log"
+	"github.com/njason/shouldwater/models"
+	"gopkg.in/yaml.v3"
+)
+
+
+const PrecipitationDataType = "PRCP"
+
+type NcdcConfig struct {
+	Url string `yaml:"ncdcUrl"`
+	Token string `yaml:"token"`
+	DatasetId string `yaml:"datasetId"`
+	Limit string `yaml:"limit"`
+}
+
+type NcdcResponse struct {
+	Metadata struct {
+		Resultset struct {
+			Offset int `json:"offset"`
+			Count  int `json:"count"`
+			Limit  int `json:"limit"`
+		} `json:"resultset"`
+	} `json:"metadata"`
+	Results []struct {
+		Date       string `json:"date"`
+		Datatype   string `json:"datatype"`
+		Station    string `json:"station"`
+		Attributes string `json:"attributes"`
+		Value      int    `json:"value"`
+	} `json:"results"`
+}
+
+func loadConfig(configFile string) NcdcConfig {
+	f, err := os.Open(configFile)
+	if err != nil {
+		fmt.Println(err.Error(), http.StatusBadRequest)
+	}
+
+	defer f.Close()
+
+	var config NcdcConfig	
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(&config)
+	if err != nil {
+		fmt.Println(err.Error(), http.StatusBadRequest)
+	}
+
+	return config
+}
+
+func getQueryDateTimeFormat(time time.Time) string {
+	return fmt.Sprintf("%d-%02d-%02d", time.Year(), time.Month(), time.Day())
+}
+
+func buildNcdcRequest(
+	stationId string,
+	daysToRequest int) *http.Request {
+
+	config := loadConfig("config.yaml")
+
+	today := time.Now()
+	endDate := today.AddDate(0, 0, -1)
+	startDate := endDate.AddDate(0, 0, -6)
+
+	req, _ := http.NewRequest("GET", config.Url, nil)
+	req.Header.Add("token", config.Token)
+	query := req.URL.Query()
+	query.Add("datasetid", config.DatasetId)
+	query.Add("stationid", fmt.Sprintf("%s:%s", config.DatasetId, stationId))
+	query.Add("startdate", getQueryDateTimeFormat(startDate))
+	query.Add("enddate", getQueryDateTimeFormat(endDate))
+	query.Add("limit", config.Limit)
+	req.URL.RawQuery = query.Encode()
+
+	return req
+}
+
+func getNCDCData(
+	stationId string,
+	daysToRequest int) NcdcResponse {
+
+		req := buildNcdcRequest(stationId, daysToRequest)
+	client := http.DefaultClient
+	rawResp, _ := client.Do(req)
+	defer rawResp.Body.Close()
+
+	var resp NcdcResponse
+	err := json.NewDecoder(rawResp.Body).Decode(&resp)
+	if err != nil {
+		fmt.Println(err.Error(), http.StatusBadRequest)
+		log.Fatal()
+	}
+
+	return resp
+}
+
+// convertToInch will translate the NCDC value for precipitation, which is tenths of mm, to inches
+func convertToInch(value int) float64 {
+	return math.Round(float64(value)/10/25.4*100) / 100
+}
+
+func GetNCDCRainfall(
+	stationId string,
+	daysToRequest int) [7]models.RainfallDate {
+
+	ncdcData := getNCDCData(stationId, daysToRequest)
+	var rainfallPerDay [7]models.RainfallDate
+
+	dayCounter := 0
+	for _, result := range ncdcData.Results {
+		fmt.Println(result)
+		if result.Datatype == PrecipitationDataType {
+			var rainyDay models.RainfallDate
+			rainyDay.Day,_ = time.Parse("YYYY-MM-DD", result.Date)
+			rainyDay.RainfallInInches = convertToInch(result.Value)
+
+			rainfallPerDay[dayCounter] = rainyDay
+			dayCounter += 1
+		}
+	}
+
+	return rainfallPerDay
+}
--- main.go
+++ main.go
@@ -1,66 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"math"
-	"net/http"
 	"os"
-	"time"
-
-	"gopkg.in/yaml.v3"
+	"github.com/njason/shouldwater/dataproviders"
 )
 
-type Config struct {
-	Token string `yaml:"token"`
-}
-
-type NcdcResponse struct {
-	Metadata struct {
-		Resultset struct {
-			Offset int `json:"offset"`
-			Count  int `json:"count"`
-			Limit  int `json:"limit"`
-		} `json:"resultset"`
-	} `json:"metadata"`
-	Results []struct {
-		Date       string `json:"date"`
-		Datatype   string `json:"datatype"`
-		Station    string `json:"station"`
-		Attributes string `json:"attributes"`
-		Value      int    `json:"value"`
-	} `json:"results"`
-}
-
-const PrecipitationDataType = "PRCP"
 const WateringThreshold = 1  // in inches
-
-// convertToInch will translate the NCDC value for precipitation, which is tenths of mm, to inches
-func convertToInch(value int) float64 {
-	return math.Round(float64(value)/10/25.4*100) / 100
-}
-
-func getQueryFormat(time time.Time) string {
-	return fmt.Sprintf("%d-%02d-%02d", time.Year(), time.Month(), time.Day())
-}
-
-func loadConfig() Config {
-	f, err := os.Open("config.yaml")
-	if err != nil {
-		fmt.Println(err.Error(), http.StatusBadRequest)
-	}
-	defer f.Close()
-
-	var config Config
-	decoder := yaml.NewDecoder(f)
-	err = decoder.Decode(&config)
-	if err != nil {
-		fmt.Println(err.Error(), http.StatusBadRequest)
-	}
-
-	return config
-}
 
 func main() {
 	flag.Parse()
@@ -69,49 +16,24 @@ func main() {
 		os.Exit(1)
 	}
 
-	config := loadConfig()
+	fmt.Println("Done loading config.");
 	stationId := flag.Arg(0)
 
-	client := http.DefaultClient
+	fmt.Println("StationId: ", stationId);
+	daysToRequest := 7
+	
+	// Get NCDC Precipitation
+	ncdcRainfall := dataproviders.GetNCDCRainfall(stationId, daysToRequest)	
+	
+	fmt.Println(ncdcRainfall)
 
-	today := time.Now()
-	end := today.AddDate(0, 0, -1)
-	start := end.AddDate(0, 0, -6)
-
-	req, _ := http.NewRequest("GET", "https://www.ncdc.noaa.gov/cdo-web/api/v2/data", nil)
-	req.Header.Add("token", config.Token)
-	query := req.URL.Query()
-	query.Add("datasetid", "GHCND")
-	query.Add("stationid", fmt.Sprintf("GHCND:%s", stationId))
-	query.Add("startdate", getQueryFormat(start))
-	query.Add("enddate", getQueryFormat(end))
-	query.Add("limit", "1000")
-	req.URL.RawQuery = query.Encode()
-
-	rawResp, _ := client.Do(req)
-	defer rawResp.Body.Close()
-
-	var resp NcdcResponse
-	err := json.NewDecoder(rawResp.Body).Decode(&resp)
-	if err != nil {
-		fmt.Println(err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	totalPrecipRaw := 0
-	dayCounter := 0
-
-	for _, result := range resp.Results {
-		if result.Datatype == PrecipitationDataType {
-			totalPrecipRaw += result.Value
-			dayCounter++
-		}
-	}
-
-	if dayCounter < 7 {
+	if len(ncdcRainfall) < 7 {
 		fmt.Println("Insufficient data to total rain")
 	} else {
-		totalPrecip := convertToInch(totalPrecipRaw)
+		totalPrecip := 0.0
+		for _, rainyDay := range ncdcRainfall {
+			totalPrecip += rainyDay.RainfallInInches
+		}
 		
 		fmt.Printf("It's rained %.2f inches in the last week\n", totalPrecip)
 		if totalPrecip < WateringThreshold {
